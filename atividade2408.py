2450bec616d02aab3dbe8db74f5a2048a786a9c3
gabvasconceloss <gabrivasc2411@gmail.com>
2026-08-24 11:24:25 -0400

    inicializando repo

---
+++ atividade2408.py
@@ -0,0 +1,52 @@
+""" Questão 1 | Faça um Script em Python que apenas imprima o seu nome na tela e em seguida finalize a
+aplicação. """
+
+#nome = "Gabriel"
+#print(nome)
+
+""" Questão 2 | Faça um Script em Python que solicite ao usuário digitar o seu nome e em seguida envie a seguinte
+frase para a saída padrão: "O seu nome é: [nome do usuário]". """
+
+#nome = input("Digite o seu nome: ")
+#print("O seu nome é: ",nome)
+
+""" Questão 3 | Faça um Script em Python que solicite o nome e a idade do usuário e então, envie a seguinte frase
+para o Console: "O seu nome é <nome> e a sua idade é <idade>". """
+
+#nome = input("Digite o seu nome: ")
+#idade = int(input("Digite a sua idade: "))
+
+#print("O seu nome é: ",format(nome), "e sua idade é: ", format(idade))
+
+""" Questão 4 | Faça um Script em Python que solicite ao usuário informar um número. Em seguida, armazene
+este número numa variável e por fim, mostre o dobro e a metade do valor digitado. """
+
+#num1 = float(input("Digite um número: "))
+
+#print("O dobro do numero que você digitou é : ",format(num1*2),"e a metade deste valor é: ",format(num1/2))
+
+""" Questão 5| Faça um Script em Python que solicite ao usuário informar 3 números. Em seguida, some-os e
+envie para a saída padrão a seguinte frase: "A soma total é: " """
+
+#n1, n2, n3 = float(input("Digite o primeiro numero: ")), float(input("Digite o segundo numero: ")), float(input("Digite o terceiro numero: "))
+#print("A soma de todos os numeros é: ", format(n1+n2+n3))
+
+""" Faça um Script em Python que solicite ao usuário informar 3 números. Em seguida, multiplique os
+valores e envie para a saída padrão a seguinte frase: "A multiplicação entre <X>, <Y> e <Z> é igual
+<Total>". """
+
+#n1, n2, n3 = float(input("Digite o primeiro numero: ")), float(input("Digite o segundo numero: ")), float(input("Digite o terceiro numero: "))
+#print("A multiplicação entre ",format(n1),", ",format(n2)," e ",format(n3)," é igual a: ", format(n1*n2*n3))
+
+""" Questão 7 | Faça um Script em Python que solicite a nota das 4 provas semestrais de um aluno. Em seguida,
+calcule e envie para a saída padrão a média. """
+
+#nota1, nota2, nota3, nota4 = float(input("Digite a primeira nota: ")), float(input("Digite a segunda nota: ")), float(input("Digite a terceira nota: ")), float(input("Digite a quarta nota: "))
+#print("A média das notas é: ", format((nota1+nota2+nota3+nota4)/4))
+
+""" Questão 8 |Faça um Script em Python em que o usuário informe uma medida em metros. Em seguida, converta
+essa medida para centímetros e envie para a saída padrão. """
+
+#medida = float(input("Insira a medida em metros: "))
+#print("Está medida em centimetros é: ",format(medida*100),"cm.")
+
---
+++ teste.py
@@ -0,0 +1,35 @@
+""" x, y, z = "laranja", "azul", "verde"
+print(x)
+print(y)
+print(z) """
+
+""" faturamento: float = 2000
+custo: float = 500
+
+lucro: float = faturamento - custo
+
+# Usando str() para converter valores em strings ao imprimir
+print('O faturamento da loja foi de R$ ' + str(faturamento))
+print('O custo da loja foi de R$ ' + str(custo))
+print('O lucro da loja foi de R$ ' + str(lucro)) """
+
+""" x = int(6)
+y = int(5.5)
+z = int("8")
+print(x)
+print(y)
+print(z)
+
+soma = x + y + z
+print("A soma dos valores é: {}".format(soma)) """
+
+""" nota1 = input("Digite a primeira nota: ")
+nota2 = input("Digite a segunda nota: ")
+
+print("A primeira nota digitada foi: " + nota1)
+print("A segunda nota digitada foi: " + nota2)
+print("A soma das notas é: " + str(float(nota1) + float(nota2))) """
+
+
+""" x = "Fácil de aprender"
+print("Python é " + x) """
